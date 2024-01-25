d4e9d10dfe7931df2efbaa86148a83702d081d47
GilbertoMG <gilbmg@gmail.com>
2024-01-24 21:38:32 -0300

    teste

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
     exit($message);
 }
 
+
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
